06aceea5420147feb2fd2d076ee74846d5d763b7
iv4n89 <ibetanzos@aiudo.es>
2023-06-10 23:29:08 +0200

    testing beneficiary card

--- pages/user/all.tsx
+++ pages/user/all.tsx
@@ -1,17 +1,29 @@
 import userApi from "@/api/user.api";
 import { Layout } from "@/base/Layout";
+import { FormControlBox } from "@/components/boxes/FormControlBox";
 import { User } from "@/interfaces/user.interface";
-import { Chip, Typography } from "@mui/material";
-import { Card, Container, Grid } from "@nextui-org/react";
+import { Chip, TextField, Typography } from "@mui/material";
+import { Card, Container, Grid, Modal, Text } from "@nextui-org/react";
 import { useEffect, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 
 
 export default function AllUsers() {
 
     const [users, setUsers] = useState([] as User[]);
+    const [modalOpen, setModalOpen] = useState(false);
+    const { control, watch, handleSubmit } = useForm();
+    const [editUser, setEditUser] = useState({} as User);
     useEffect(() => {
         userApi.getAllUsers().then(setUsers);
     }, []);
+
+    const onsubmit = (data: any) => {
+        userApi.updateUser(editUser.id!, data).then(res => {
+            setUsers(elements => elements.map(e => e.id === editUser.id ? res : e));
+            setModalOpen(false);
+        })
+    }
 
     return (
         <Layout>
@@ -23,6 +35,10 @@ export default function AllUsers() {
                                 <Card 
                                     isHoverable
                                     isPressable
+                                    onClick={() => {
+                                        setModalOpen(true);
+                                        setEditUser(user);
+                                    }}
                                 >
                                     <Card.Header
                                         style={{ 
@@ -84,6 +100,85 @@ export default function AllUsers() {
                                         }
                                     </Card.Body>
                                 </Card>
+                                <Modal
+                                    closeButton
+                                    open={modalOpen}
+                                    onClose={() => setModalOpen(false)}
+                                >
+                                    <Modal.Header>
+                                        <Text b size={10}>
+                                            Editar beneficiario
+                                        </Text>
+                                    </Modal.Header>
+                                    <Modal.Body>
+                                        <form onSubmit={handleSubmit(onsubmit)}>
+                                            <FormControlBox>
+                                                <Controller
+                                                    control={control}
+                                                    name='name'
+                                                    render={
+                                                        ({field}) => (
+                                                            <TextField
+                                                                {...field}
+                                                                color='secondary'
+                                                                label='Nombre'
+                                                                defaultValue={editUser.name}
+                                                            />
+                                                        )
+                                                    }
+                                                />
+                                            </FormControlBox>
+                                            <FormControlBox>
+                                                <Controller
+                                                    control={control}
+                                                    name='surname'
+                                                    render={
+                                                        ({field}) => (
+                                                            <TextField
+                                                                {...field}
+                                                                color='secondary'
+                                                                label='Apellidos'
+                                                                defaultValue={editUser.surname}
+                                                            />
+                                                        )
+                                                    }
+                                                />
+                                            </FormControlBox>
+                                            <FormControlBox>
+                                                <Controller
+                                                    control={control}
+                                                    name='telNumber'
+                                                    render={
+                                                        ({field}) => (
+                                                            <TextField
+                                                                {...field}
+                                                                color='secondary'
+                                                                label='Teléfono'
+                                                                defaultValue={editUser.telNumber}
+                                                            />
+                                                        )
+                                                    }
+                                                />
+                                            </FormControlBox>
+                                            <FormControlBox>
+                                                <Controller
+                                                    control={control}
+                                                    name='indentityDoc.idNumber'
+                                                    render={
+                                                        ({field}) => (
+                                                            <TextField
+                                                                {...field}
+                                                                color='secondary'
+                                                                label='Nombre'
+                                                                defaultValue={editUser.identityDoc?.idNumber}
+                                                            />
+                                                        )
+                                                    }
+                                                />
+                                            </FormControlBox>
+                                        </form>
+                                    </Modal.Body>
+                                </Modal>
                             </Grid>
                         ))
                     }
