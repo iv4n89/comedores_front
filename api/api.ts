83184b9e78ed43fcecf287edc30f766d36725402
Iván Betanzos Macías <ivan.betanzos89@gmail.com>
2023-06-14 23:44:53 +0200

    Update api.ts

--- api/api.ts
+++ api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://comedores-back.herokuapp.com/'
+    baseURL: 'https://comedores-back.herokuapp.com/' 
     //baseURL: 'http://localhost:4000/'
 });
